b37aaa9ef8af8847fd479ad5d4aa3e58655dea5b
huntingmarco <laquihonjannel@yahoo.com>
2021-06-19 00:04:40 -0400

    Apply Routes, Component, mode

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/{vue_capture?}',function () {
+    return view('welcome');
+})->where('vue_capture', '[\/\w\.-]*');
+
 // Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
